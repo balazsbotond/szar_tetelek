9e2e5fc8b77a71493093c99bdc6a43484818a1e5
Botond Balázs <balazsbotond@gmail.com>
2013-12-15 16:55:43 +0100

    2.1 kesz

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 scp /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/*.tex botond@botond.me:/home/botond/szar_tetelek/
-ssh botond@botond.me "cd /home/botond/szar_tetelek;latex szar_tetelek.tex;dvipdf szar_tetelek.dvi szar_tetelek.pdf"
+scp /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/*.png botond@botond.me:/home/botond/szar_tetelek/
+ssh botond@botond.me "cd /home/botond/szar_tetelek;pdflatex szar_tetelek.tex"
 scp botond@botond.me:/home/botond/szar_tetelek/*.pdf /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/
