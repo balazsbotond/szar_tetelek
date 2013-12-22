4dc87ab2db5cc9ec7b16292934066443691b117b
Botond Balázs <balazsbotond@gmail.com>
2013-12-22 21:35:52 +0100

    Kepek img konyvtarba mozgatva

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 scp /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/*.tex botond@botond.me:/home/botond/szar_tetelek/
-scp /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/*.png botond@botond.me:/home/botond/szar_tetelek/
+scp -r /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/img botond@botond.me:/home/botond/szar_tetelek/
 ssh botond@botond.me "cd /home/botond/szar_tetelek;pdflatex szar_tetelek.tex"
 scp botond@botond.me:/home/botond/szar_tetelek/*.pdf /Users/botondbalazs/Dropbox/School/SZTE/szamitogep-architekturak/szar_tetelek/
